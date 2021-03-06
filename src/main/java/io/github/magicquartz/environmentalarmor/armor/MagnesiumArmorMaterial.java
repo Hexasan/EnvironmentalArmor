d1faf7546541d5aa3b723cbf5d2dff345926b326
Kesem Hershcovich <kesem100k@gmail.com>
2021-03-06 05:01:25 +0200

    Added crafting recipes for magnesium.

--- src/main/java/io/github/magicquartz/environmentalarmor/armor/MagnesiumArmorMaterial.java
+++ src/main/java/io/github/magicquartz/environmentalarmor/armor/MagnesiumArmorMaterial.java
@@ -1,19 +1,26 @@
 package io.github.magicquartz.environmentalarmor.armor;
 
+import io.github.magicquartz.environmentalarmor.registry.ModItems;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 
 public class MagnesiumArmorMaterial implements ArmorMaterial {
+
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {1, 1, 1, 1};
+
     @Override
     public int getDurability(EquipmentSlot slot) {
-        return 0;
+        return BASE_DURABILITY[slot.getEntitySlotId()] * 30;
     }
 
     @Override
     public int getProtectionAmount(EquipmentSlot slot) {
-        return 0;
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
     }
 
     @Override
@@ -23,26 +30,26 @@ public class MagnesiumArmorMaterial implements ArmorMaterial {
 
     @Override
     public SoundEvent getEquipSound() {
-        return null;
+        return SoundEvents.ITEM_ARMOR_EQUIP_CHAIN;
     }
 
     @Override
     public Ingredient getRepairIngredient() {
-        return null;
+        return Ingredient.ofItems(ModItems.MAGNESIUM_INGOT);
     }
 
     @Override
     public String getName() {
-        return null;
+        return "magnesium";
     }
 
     @Override
     public float getToughness() {
-        return 0;
+        return 1.5F;
     }
 
     @Override
     public float getKnockbackResistance() {
-        return 0;
+        return 0.2F;
     }
 }
